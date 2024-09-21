58989f2c056821dfe7f08ce8dfeecccd9eb862b1
Muhammad Syarif Hidayatullah <syarifhidayatullah05052006@gmail.com>
2024-09-21 14:47:10 +0700

    Add files via upload

---
+++ latihan03.java
@@ -0,0 +1,12 @@
+public class latihan03 {
+    public static void main(String[] args) {
+        //konstanta setelah diisi tida bisa di ubah,variabel=bisa diubah    
+        System.out.println("==================================================");
+        System.out.println("No. |Nama                         |NIM           |");
+        System.out.println("=================================================="); 
+        System.out.println("1.  |Muhammad Syarif Hidayatullah |2411016310007 |");
+        System.out.println("2.  |                             |              |");
+        System.out.println("==================================================");
+    }
+}
+ 
